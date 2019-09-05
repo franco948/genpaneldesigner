0b1a9fec073298a3c69e1fabed0fa14d63a2fd80
fpalermo <franco.palermo812@gmail.com>
2019-09-05 19:33:00 -0300

    Agregar un hola mundo para verificar funcionamiento

--- src/main/java/ar/edu/uai/genpaneldesigner/GenpaneldesignerApplication.java
+++ src/main/java/ar/edu/uai/genpaneldesigner/GenpaneldesignerApplication.java
@@ -2,9 +2,20 @@ package ar.edu.uai.genpaneldesigner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class GenpaneldesignerApplication {
+
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GenpaneldesignerApplication.class, args);
