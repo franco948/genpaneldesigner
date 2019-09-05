78b83ccad0b9bdfd0f91ad30979f06e93a4ab0d8
fpalermo <franco.palermo812@gmail.com>
2019-09-05 13:22:54 -0300

    Inicializar repositorio

---
+++ src/main/java/ar/edu/uai/genpaneldesigner/GenpaneldesignerApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.uai.genpaneldesigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenpaneldesignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenpaneldesignerApplication.class, args);
+	}
+
+}
